f3f36dc66d78f077ab9563d47e0053a37fdb2652
abubakar308 <mdabubakarsiddique789@gmail.com>
2025-07-09 15:09:30 +0600

    feat card width problem fix for mobile

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -2,10 +2,11 @@ import { Link } from "react-router";
 
 const ProductCard = ({ product }) => {
     return (
-      <div key={product.id} className="card w-[200px] bg-base-100 shadow-sm border hover:shadow-md transition-all">
+      <div key={product.id} className="card w-full sm:w-[200px] bg-base-100 shadow-sm border hover:shadow-md transition-all">
       <figure className="px-4 pt-4">
         <img
           src={product.image}
+          
           alt={product.title}
           className="rounded-xl h-32 object-contain"
         />
