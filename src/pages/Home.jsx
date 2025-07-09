23f599ab29d5a267f9637222dff6df2a462472c1
abubakar308 <mdabubakarsiddique789@gmail.com>
2025-07-09 15:34:43 +0600

    feat: hero section banner added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,23 @@
 import ProductList from "../components/ProductList";
+import banner from '../assets/banner.jpg'
 
 const Home = () => {
     return (
        <div className="space-y-8">
-      {/* Optional Hero */}
-      <section className="text-center">
-        <h1 className="text-3xl font-bold text-indigo-600">Welcome to TinyMart</h1>
-        <p className="text-gray-600">Your one-stop shop for awesome products!</p>
+     {/* Hero Section optional */}
+      <section className="relative">
+        <img
+          src={banner}
+          alt="TinyMart Banner"
+           className="w-full h-48 sm:h-60 md:h-72 lg:h-96 object-cover rounded-lg"
+        />
+        <div className="absolute inset-0 bg-black/40 flex flex-col items-center justify-center text-white text-center px-4">
+          <h1 className="text-4xl font-bold">Welcome to TinyMart</h1>
+          <p className="text-lg">Your one-stop shop for awesome products!</p>
+        </div>
       </section>
 
-      {/* Required: Product List Section */}
+      {/* Product List Section */}
       <ProductList />
     </div>
     );
